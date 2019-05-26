6c1c7653c7720b67069329b8f284b539a33d61d5
Starcode71Ooze <shivangisingh1702@gmail.com>
2019-05-26 13:17:40 +0530

    Check Connection

--- AmritaResource/src/main/java/com/example/android/AmritaResouce/MainActivity.java
+++ AmritaResource/src/main/java/com/example/android/AmritaResouce/MainActivity.java
@@ -149,3 +149,32 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 }
+
+public class InternetConnection {
+
+    /** CHECK WHETHER INTERNET CONNECTION IS AVAILABLE OR NOT */
+    public static boolean checkConnection(Context context) {
+        final ConnectivityManager connMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetworkInfo = connMgr.getActiveNetworkInfo();
+
+        if (activeNetworkInfo != null) { // connected to the internet
+            Toast.makeText(context, activeNetworkInfo.getTypeName(), Toast.LENGTH_SHORT).show();
+
+            if (activeNetworkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+                // connected to wifi
+                return true;
+            } else if (activeNetworkInfo.getType() == ConnectivityManager.TYPE_MOBILE) {
+                // connected to the mobile provider's data plan
+                return true;
+            }
+        }
+        return false;
+    }
+}
+if (InternetConnection.checkConnection(context)){
+        Toast.makeText(MainActivity.this,"Network Available",Toast.LENGTH_LONG).show();
+        }
+else{
+        Toast.makeText(MainActivity.this, "Network Not Available", Toast.LENGTH_LONG).show();
+        }

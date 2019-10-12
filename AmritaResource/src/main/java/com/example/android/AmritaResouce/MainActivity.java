ae2c6ecf2c1b51b237b2861297ebe97f45df8f1c
root-ansh <anshsachdevaprofessional@gmail.com>
2019-10-12 18:27:00 +0530

    added ARCA(crash report via mail) support

--- AmritaResource/src/main/java/com/example/android/AmritaResouce/MainActivity.java
+++ AmritaResource/src/main/java/com/example/android/AmritaResouce/MainActivity.java
@@ -41,13 +41,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Find the View that shows the numbers category
-        TextView numbers = (TextView) findViewById(R.id.numbers);
-
-
+        TextView numbers = findViewById(R.id.numbers);
 
 
         //getting the toolbar
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
 
         //setting the title
         toolbar.setTitle("Amrita Resource");
@@ -73,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Find the View that shows the family category
-        TextView family = (TextView) findViewById(R.id.family);
+        TextView family = findViewById(R.id.family);
 
         // Set a click listener on that View
         family.setOnClickListener(new OnClickListener() {
@@ -89,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Find the View that shows the colors category
-        TextView colors = (TextView) findViewById(R.id.colors);
+        TextView colors = findViewById(R.id.colors);
 
         // Set a click listener on that View
         colors.setOnClickListener(new OnClickListener() {
@@ -105,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Find the View that shows the phrases category
-        TextView phrases = (TextView) findViewById(R.id.phrases);
+        TextView phrases = findViewById(R.id.phrases);
 
         // Set a click listener on that View
         phrases.setOnClickListener(new OnClickListener() {

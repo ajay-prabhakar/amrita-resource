82552f37e23a7fbe988cd90005f1d708433d20e9
root-ansh <anshsachdevaprofessional@gmail.com>
2019-10-14 00:01:09 +0530

    some more changes

--- AmritaResource/src/main/java/com/example/android/AmritaResouce/MainApplication.java
+++ AmritaResource/src/main/java/com/example/android/AmritaResouce/MainApplication.java
@@ -15,7 +15,8 @@ import org.acra.data.StringFormat;
 @AcraMailSender(mailTo = MainApplication.EMAIL_TO)
 @AcraCore(buildConfigClass = BuildConfig.class)
 public class MainApplication extends Application {
-    public static final String EMAIL_TO= "email@provider.bla";//replace this value with your own(owner of the app) email id
+    public static final String EMAIL_TO =
+            "email@provider.bla"; // replace this value with your own(owner of the app) email id
 
     @Override
     protected void attachBaseContext(Context base) {
@@ -29,7 +30,8 @@ public class MainApplication extends Application {
                 .setEnabled(true)
                 .setMailTo(EMAIL_TO)
                 .setSubject("Crash occurred on user device.");
-        builder.getPluginConfigurationBuilder(ToastConfigurationBuilder.class)
+        builder
+                .getPluginConfigurationBuilder(ToastConfigurationBuilder.class)
                 .setResText(R.string.crash_toast_msg);
 
         ACRA.init(this, builder);

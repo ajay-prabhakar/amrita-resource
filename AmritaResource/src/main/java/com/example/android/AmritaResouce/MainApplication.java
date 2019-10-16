d5dd44c96d59b84c17e9aac4263bf25a25dad70d
Ansh Sachdeva <anshsachdevaprofessional@gmail.com>
2019-10-16 16:24:44 +0530

    added design changes and colors for fixing #8 and also added ACRA Framework for Fixing #1 (#17)
    * added design changes and colors for fixing #8
    
    * added ACRA framework for crash reporting via mail for fixing #1
    
    * some more changes
    
    * ran all travis ci steps locally, applied changes

---
+++ AmritaResource/src/main/java/com/example/android/AmritaResouce/MainApplication.java
@@ -0,0 +1,38 @@
+package com.example.android.AmritaResouce;
+
+import android.app.Application;
+import android.content.Context;
+import org.acra.ACRA;
+import org.acra.annotation.AcraCore;
+import org.acra.annotation.AcraMailSender;
+import org.acra.config.CoreConfigurationBuilder;
+import org.acra.config.MailSenderConfigurationBuilder;
+import org.acra.config.ToastConfigurationBuilder;
+import org.acra.data.StringFormat;
+
+@AcraMailSender(mailTo = MainApplication.EMAIL_TO)
+@AcraCore(buildConfigClass = BuildConfig.class)
+public class MainApplication extends Application {
+    public static final String EMAIL_TO =
+            "email@provider.bla"; // replace this value with your own(owner of the app) email id
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+
+        CoreConfigurationBuilder builder = new CoreConfigurationBuilder(this);
+        builder.setBuildConfigClass(BuildConfig.class).setReportFormat(StringFormat.JSON);
+
+        builder
+                .getPluginConfigurationBuilder(MailSenderConfigurationBuilder.class)
+                .setEnabled(true)
+                .setMailTo(EMAIL_TO)
+                .setSubject("Crash occurred on user device.");
+        builder
+                .getPluginConfigurationBuilder(ToastConfigurationBuilder.class)
+                .setResText(R.string.crash_toast_msg);
+
+        ACRA.init(this, builder);
+        // The following line triggers the initialization of ACRA
+    }
+}
